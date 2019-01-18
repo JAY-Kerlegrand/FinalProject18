236d5f71e1ea9a809c23304970fecd37918b1309
jay <jkerlegrand@ucvts.org>
2019-01-18 15:47:45 +0000

    fully commented code

--- beginnings.py
+++ beginnings.py
@@ -26,16 +26,17 @@ for line in intro:
     time.sleep(2)
 print("-----------------")
 
-#each character has different stats of base energy, work done per click, and base happiness
 class Magneteer:
 
     def __init__(self, name, energy, work, happiness):
+        """A class to create Magnet students; each of which has a name and different stats of base energy, work done per click, and base happiness; This class also contains to functions to perform different tasks to raise/lower different stats"""
         self.name = name
         self.energy = energy
         self.work = work
         self.happiness = happiness
 
     def stats(self):
+        """A function to display your progress throughout the game"""
         global WORK
         global ENERGY
         global HAPPINESS
@@ -55,6 +56,7 @@ class Magneteer:
         \033[1m{ENERGY}\033[0m ENERGY left
         \033[1m{HAPPINESS}\033[0m HAPPINESS
         \033[1m{NUM_CLICKS}\033[0m CLICKS left""")
+        #A warning is issued if any of your stats are too low
         if WORK < 150:
             if ENERGY <= 5:
                 print("Woah, there buddy; you're low on ENERGY. Maybe you should sleep(s) or eat(e)?")
@@ -64,8 +66,8 @@ class Magneteer:
                 print("You're running out of CLICKS there, man! Prioritize getting that WORK done!!")
         print("\n")
 
-
     def working(self):
+        """A function to do an amount of work, the rate of which is different for every character; other stats also go down as a result"""
         global WORK
         global ENERGY
         global HAPPINESS
@@ -77,6 +79,7 @@ class Magneteer:
         NUM_CLICKS -= 1
 
     def eat(self):
+        """A function to eat and restore energy"""
         global ENERGY
         global NUM_CLICKS
         eating = ["Mother made some porridge, how nice.", "Crackers and cheese replenish your soul.", "Let's take a break for some grapes.", "You eat a cinnamon bun. It's sweet, just like you <3.", "Mmmm, sesame candy...", "You...drink some water? That's it? Ok..."]
@@ -86,6 +89,7 @@ class Magneteer:
         NUM_CLICKS -= 1
 
     def sleep(self):
+        """A function to sleep and restore a large amount of energy, but lose a random amount of clicks from 3-5"""
         global NUM_CLICKS
         global ENERGY
         print("You catch a few winks. But at what cost? You lost some CLICKS to do your WORK!")
@@ -93,6 +97,7 @@ class Magneteer:
         NUM_CLICKS -= random.randint(3,5)
 
     def procrastinate(self):
+        """A function to procrastinate and restore happiness; lowers energy and number of clicks"""
         global ENERGY
         global HAPPINESS
         global NUM_CLICKS
@@ -104,7 +109,7 @@ class Magneteer:
         NUM_CLICKS -= 1
 
     def cry(self):
-        """The cry function has a 50/50 chance of either increasing or decreasing your HAPPINESS"""
+        """A function to cry which has a 50/50 chance of either increasing or decreasing your happiness"""
         global HAPPINESS
         global NUM_CLICKS
         p = [1, 2]
@@ -117,11 +122,13 @@ class Magneteer:
             HAPPINESS -= 3
         NUM_CLICKS -= 1
 
+#A dictionary to contain the three characters and make it easy to select them
 characters = {
     "owl": Magneteer("Night Owl", 70, 6, 50),
     "genius": Magneteer("MIT Genius", 50, 8, 40),
     "studious": Magneteer("Studious Student", 40, 7, 70)}
 
+#A short description of each character type
 print("What type of Magneteer are you? \n")
 time.sleep(2)
 #I have used Unicode to bold certain words
@@ -132,6 +139,7 @@ time.sleep(2)
 print(f"★★ The \033[1m Studious Student \033[0m has less ENERGY, but doesn't need to procrastinate as much. They start off with {characters['studious'].energy} ENERGY and {characters['studious'].happiness} HAPPINESS, and get {characters['studious'].work} WORK done per click.\n")
 time.sleep(2)
 
+#player chooses what character to play as
 char = input("So, how do you WORK? (owl)(genius)(studious)\n").lower()
 while char not in characters:
     char = input("So, how do you WORK? (owl)(genius)(studious)\n").lower()
@@ -139,16 +147,17 @@ while char not in characters:
 print(f"\nNice! So you're the {characters[char].name} type, huh?\n")
 time.sleep(2)
 
+#To count how many wins you get; max is 5
 WINS = 0
-LOSSES = 0
 
-#These are quick directions
+#Quick directionson how to play the game
 directions = ["Directions:", "Press (e) to eat", "Press (s) to sleep", "Press (p) to procrastinate", "Press (c) to... cry", "Be careful, sleeping and crying may have some consequences...", "Stragetize! You've only got 50 CLICKS to get it done, so use 'em wisely!"]
 for line in directions:
     print(line)
     time.sleep(1)
 print("\n")
 
+#For each day of the week, a "night" is played (a loop of 5 turns); there is a different "start game" line depending on the day
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 for day in days:
     if day == 'Monday':
@@ -158,11 +167,13 @@ for day in days:
     else:
         print(f"It's the next night, {day}. Let's get to WORK!")
 
+    #Stats reset for every night
     NUM_CLICKS = 50
     WORK = 0
     ENERGY = characters[char].energy
     HAPPINESS = characters[char].happiness
 
+    #Within "weekday loop" there is a loop of at most 50 turns(the max number of "clicks") that asks the player what action to perform until work is completed or a stat is depleted
     while WORK < 150:
         if NUM_CLICKS <= 0:
             print("Fiddlesticks! You ran out of CLICKS to do your WORK!\n")
@@ -190,6 +201,7 @@ for day in days:
             characters[char].cry()
             characters[char].stats()
 
+    #When work is complete or a stat is depleted, a win or lose line is displayed, respectively; the lose line is different if the day is Friday
     time.sleep(2)
     if WORK == 150:
         print("Homework COMPLETED! You finished all of your night's WORK, with your sanity still intact! Well, mostly intact...\n")
@@ -209,19 +221,20 @@ print(".")
 time.sleep(2)
 print("\n")
 
+#Total win nights are counted up, and depending on how many wins the player gets, they receive a different "grade"; you cannot earn a 100, because life's not fair sometimes, bro
 print(f"Well, {characters[char].name}, you've completed {WINS} day's work out of 5.")
 if WINS == 0:
     print("LMAO buddy, you ok? You got a whole-ass 0 on, like, EVERY assignment. Like, were you just playing Deltarune the entire week or something? How did you even get into Magnet??")
 elif WINS == 1:
-    print("You got a..54? Just didn't care, huh? Can't blame ya, I guess, who needs to know what an 'epithet' is, amirite? ...No, actually, I'm really disappointed in you; please try harder next time, sweety.")
+    print("You got a..54? Just didn't care, huh? Can't blame ya, I guess, who needs to know what a derivative is, amirite? ...No, actually, I'm really disappointed in you; please try harder next time, sweety.")
 elif WINS == 2:
-    print("Welp, that's a 68 in your gradebook, lmao. Just wasn't your luck, huh? Eh, you've still got a week till the marking period ends. Hopefully there's some extra credit oppurtunities...")
+    print("Welp, that's a 68 in your gradebook, lmao. Just wasn't your luck, huh? Eh, you've still got a week till the marking period ends. Hopefully, there's some extra credit oppurtunities...")
 elif WINS == 3:
     print("OOF, that there's a 75 perciento. Lo siento, but you've gotta do better than that! Maybe Señora will extended the due date, and you can cop some extra puntos.")
 elif WINS == 4:
     print("Ok, ok, you got a 92. Not too shabby for bum-rushing that chem packet at one in the morning. Good job! (I mean the grade, not the procrastination)")
 elif WINS == 5:
-    print("Ayyyyyyyyyyyyyyyy, 98 FOR. THE. WIN!!! Haha, dab on 'em...*ahem* Congratulations on your grade, keep up the good work.")
+    print("Ayyyyyyyyyyyyyyyy, 98 FOR. THE. WIN!!! Haha, yEET, dab on 'em......*ahem* Congratulations on your grade, keep up the good work.")
 
 
 
